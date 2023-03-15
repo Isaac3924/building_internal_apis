545d32e2af9908f0e8021555f738807c5f1adf14
Isaac Alter <i_alter@yahoo.com>
2023-03-15 17:09:08 -0600

    Adds feat: seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+book_1 = Book.create!(title: "The Old Man and the Sea", author: "Ernest Hemingway", genre: "Novel, Allegory, Nautical fiction", summary: "The most boring story you will like.", number_sold: 5239648)
+book_2 = Book.create!(title: "Horus Rising", author: "Dan Abnett", genre: "Science fiction", summary: "Horus hears a Who.", number_sold: 3938)
+book_3 = Book.create!(title: "Trollslayer", author: "William King", genre: "Fantasy fiction", summary: "Dorf & humie seek death. Fail miserably.", number_sold: 234)
